bad7e04223c2fc609b7dbbbc5fc35c81a01d677f
JonasGao <gaopinsong@live.cn>
2022-12-16 15:46:52 +0800

    Add more features

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -33,8 +33,13 @@
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.queryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.killClashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.显示输出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.隐藏输出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,25 +57,36 @@
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
+            this.queryToolStripMenuItem,
+            this.显示输出ToolStripMenuItem,
+            this.隐藏输出ToolStripMenuItem,
+            this.killClashToolStripMenuItem,
             this.restartToolStripMenuItem,
             this.closeToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 180);
             this.contextMenuStrip1.Text = "11111111";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openToolStripMenuItem.Text = "打开";
+            this.openToolStripMenuItem.Text = "打开窗口";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
-            // closeToolStripMenuItem
+            // queryToolStripMenuItem
             // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.closeToolStripMenuItem.Text = "退出";
-            this.closeToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            this.queryToolStripMenuItem.Name = "queryToolStripMenuItem";
+            this.queryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.queryToolStripMenuItem.Text = "查询进程状态";
+            this.queryToolStripMenuItem.Click += new System.EventHandler(this.QueryToolStripMenuItem_Click);
+            // 
+            // killClashToolStripMenuItem
+            // 
+            this.killClashToolStripMenuItem.Name = "killClashToolStripMenuItem";
+            this.killClashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.killClashToolStripMenuItem.Text = "关闭 Clash";
+            this.killClashToolStripMenuItem.Click += new System.EventHandler(this.KillClashToolStripMenuItem_Click);
             // 
             // restartToolStripMenuItem
             // 
@@ -79,17 +95,48 @@
             this.restartToolStripMenuItem.Text = "重新启动";
             this.restartToolStripMenuItem.Click += new System.EventHandler(this.RestartToolStripMenuItem_Click);
             // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "退出";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "使用右键菜单控制";
+            // 
+            // 显示输出ToolStripMenuItem
+            // 
+            this.显示输出ToolStripMenuItem.Name = "显示输出ToolStripMenuItem";
+            this.显示输出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.显示输出ToolStripMenuItem.Text = "显示输出";
+            // 
+            // 隐藏输出ToolStripMenuItem
+            // 
+            this.隐藏输出ToolStripMenuItem.Name = "隐藏输出ToolStripMenuItem";
+            this.隐藏输出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.隐藏输出ToolStripMenuItem.Text = "隐藏输出";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(375, 214);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,5 +147,10 @@
         private ToolStripMenuItem closeToolStripMenuItem;
         private ToolStripMenuItem openToolStripMenuItem;
         private ToolStripMenuItem restartToolStripMenuItem;
+        private ToolStripMenuItem queryToolStripMenuItem;
+        private Label label1;
+        private ToolStripMenuItem killClashToolStripMenuItem;
+        private ToolStripMenuItem 显示输出ToolStripMenuItem;
+        private ToolStripMenuItem 隐藏输出ToolStripMenuItem;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,11 +1,14 @@
 using System.Diagnostics;
+using System.Text;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        private readonly Process process = new();
-        private bool realClose = false;
+        private const string clash = "clash.exe";
+        private readonly Process _process = new();
+        private bool _realClose = false;
+        private bool _clashRunning = false;
 
         public Form1()
         {
@@ -14,27 +17,25 @@ namespace WinFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            process.StartInfo.FileName = "clash.exe";
-            process.StartInfo.CreateNoWindow = true;
-            process.Start();
+            StartupClash();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (!realClose)
+            if (!_realClose)
             {
                 e.Cancel = true;
                 Hide();
             }
             else
             {
-                process.Kill();
+                KillClash();
             }
         }
 
         private void ToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            realClose = true;
+            _realClose = true;
             Close();
         }
 
@@ -45,10 +46,48 @@ namespace WinFormsApp1
 
         private void RestartToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            process.Kill();
+            KillClash();
             Thread.Sleep(1000);
-            process.Start();
-            MessageBox.Show("已重新启动 Clash。");
+            StartupClash();
+            MessageBox.Show("Restarted Clash.");
+        }
+
+        private void QueryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int pid = _process.Id;
+            string content = new StringBuilder()
+                .Append("ID: ").AppendLine(pid.ToString())
+                .Append("SessionID: ").AppendLine(_process.SessionId.ToString())
+                .Append("Running: ").AppendLine(_clashRunning.ToString())
+                .ToString();
+            label1.Text = content;
+        }
+
+        private void StartupClash()
+        {
+            if (!File.Exists(clash))
+            {
+                MessageBox.Show("Can not found clash.exe");
+                return;
+            }
+            _clashRunning = true;
+            _process.StartInfo.FileName = clash;
+            _process.StartInfo.CreateNoWindow = true;
+            _process.Start();
+        }
+
+        private void KillClash()
+        {
+            if (_clashRunning)
+            {
+                _process.Kill();
+                _clashRunning = false;
+            }
+        }
+
+        private void KillClashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KillClash();
         }
     }
 }
