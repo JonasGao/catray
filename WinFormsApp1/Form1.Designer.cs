39c6739485d0f6f7e3a87c4064688a34775d6076
JonasGao <gaopinsong@live.cn>
2023-08-22 18:26:31 +0800

    chore: Update notifyIcon

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -47,11 +47,9 @@
             // notifyIcon1
             // 
             this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.notifyIcon1.BalloonTipText = "1111111111";
-            this.notifyIcon1.BalloonTipTitle = "222222222";
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "Clash Tray";
             this.notifyIcon1.Visible = true;
             // 
             // contextMenuStrip1
