35ffc93e11611713fb76468fa699ec6a5b777301
JonasGao <gaopinsong@live.cn>
2024-04-17 11:52:49 +0800

    Update icon and form text.

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,124 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openConsoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.queryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.configCoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.killClashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.configProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
-            this.contextMenuStrip1.SuspendLayout();
-            this.SuspendLayout();
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            openToolStripMenuItem = new ToolStripMenuItem();
+            openConsoleToolStripMenuItem = new ToolStripMenuItem();
+            queryToolStripMenuItem = new ToolStripMenuItem();
+            configCoreToolStripMenuItem = new ToolStripMenuItem();
+            configProfileToolStripMenuItem = new ToolStripMenuItem();
+            killClashToolStripMenuItem = new ToolStripMenuItem();
+            restartToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // notifyIcon1
             // 
-            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
-            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "Clash Tray";
-            this.notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("$this.Icon");
+            notifyIcon1.Text = "Clash Tray";
+            notifyIcon1.Visible = true;
             // 
             // contextMenuStrip1
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.openConsoleToolStripMenuItem,
-            this.queryToolStripMenuItem,
-            this.configCoreToolStripMenuItem,
-            this.configProfileToolStripMenuItem,
-            this.killClashToolStripMenuItem,
-            this.restartToolStripMenuItem,
-            this.closeToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 158);
-            this.contextMenuStrip1.Text = "Clash as Tray";
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { openToolStripMenuItem, openConsoleToolStripMenuItem, queryToolStripMenuItem, configCoreToolStripMenuItem, configProfileToolStripMenuItem, killClashToolStripMenuItem, restartToolStripMenuItem, closeToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(149, 180);
+            contextMenuStrip1.Text = "Clash as Tray";
             // 
             // openToolStripMenuItem
             // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.openToolStripMenuItem.Text = "打开窗口";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(148, 22);
+            openToolStripMenuItem.Text = "打开窗口";
+            openToolStripMenuItem.Click += OpenToolStripMenuItem_Click;
             // 
             // openConsoleToolStripMenuItem
             // 
-            this.openConsoleToolStripMenuItem.Name = "openConsoleToolStripMenuItem";
-            this.openConsoleToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.openConsoleToolStripMenuItem.Text = "打开控制台";
-            this.openConsoleToolStripMenuItem.Click += new System.EventHandler(this.OpenConsoleToolStripMenuItem_Click);
+            openConsoleToolStripMenuItem.Name = "openConsoleToolStripMenuItem";
+            openConsoleToolStripMenuItem.Size = new Size(148, 22);
+            openConsoleToolStripMenuItem.Text = "打开控制台";
+            openConsoleToolStripMenuItem.Click += OpenConsoleToolStripMenuItem_Click;
             // 
             // queryToolStripMenuItem
             // 
-            this.queryToolStripMenuItem.Name = "queryToolStripMenuItem";
-            this.queryToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.queryToolStripMenuItem.Text = "查询进程状态";
-            this.queryToolStripMenuItem.Click += new System.EventHandler(this.QueryToolStripMenuItem_Click);
+            queryToolStripMenuItem.Name = "queryToolStripMenuItem";
+            queryToolStripMenuItem.Size = new Size(148, 22);
+            queryToolStripMenuItem.Text = "查询进程状态";
+            queryToolStripMenuItem.Click += QueryToolStripMenuItem_Click;
             // 
             // configCoreToolStripMenuItem
             // 
-            this.configCoreToolStripMenuItem.Name = "configCoreToolStripMenuItem";
-            this.configCoreToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.configCoreToolStripMenuItem.Text = "配置 Core";
-            this.configCoreToolStripMenuItem.Click += new System.EventHandler(this.ConfigCoreToolStripMenuItem_Click);
-            //
-            // 1
-            //
-            this.configProfileToolStripMenuItem.Name = "configProfileToolStripMenuItem";
-            this.configProfileToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.configProfileToolStripMenuItem.Text = "配置 Profile";
-            this.configProfileToolStripMenuItem.Click += new System.EventHandler(this.ConfigProfileToolStripMenuItem_Click);
+            configCoreToolStripMenuItem.Name = "configCoreToolStripMenuItem";
+            configCoreToolStripMenuItem.Size = new Size(148, 22);
+            configCoreToolStripMenuItem.Text = "配置 Core";
+            configCoreToolStripMenuItem.Click += ConfigCoreToolStripMenuItem_Click;
+            // 
+            // configProfileToolStripMenuItem
+            // 
+            configProfileToolStripMenuItem.Name = "configProfileToolStripMenuItem";
+            configProfileToolStripMenuItem.Size = new Size(148, 22);
+            configProfileToolStripMenuItem.Text = "配置 Profile";
+            configProfileToolStripMenuItem.Click += ConfigProfileToolStripMenuItem_Click;
             // 
             // killClashToolStripMenuItem
             // 
-            this.killClashToolStripMenuItem.Name = "killClashToolStripMenuItem";
-            this.killClashToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.killClashToolStripMenuItem.Text = "关闭 Clash";
-            this.killClashToolStripMenuItem.Click += new System.EventHandler(this.KillClashToolStripMenuItem_Click);
+            killClashToolStripMenuItem.Name = "killClashToolStripMenuItem";
+            killClashToolStripMenuItem.Size = new Size(148, 22);
+            killClashToolStripMenuItem.Text = "关闭 Clash";
+            killClashToolStripMenuItem.Click += KillClashToolStripMenuItem_Click;
             // 
             // restartToolStripMenuItem
             // 
-            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
-            this.restartToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.restartToolStripMenuItem.Text = "重新启动";
-            this.restartToolStripMenuItem.Click += new System.EventHandler(this.RestartToolStripMenuItem_Click);
+            restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            restartToolStripMenuItem.Size = new Size(148, 22);
+            restartToolStripMenuItem.Text = "重新启动";
+            restartToolStripMenuItem.Click += RestartToolStripMenuItem_Click;
             // 
             // closeToolStripMenuItem
             // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
-            this.closeToolStripMenuItem.Text = "退出";
-            this.closeToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(148, 22);
+            closeToolStripMenuItem.Text = "退出";
+            closeToolStripMenuItem.Click += ToolStripMenuItem1_Click;
             // 
             // label1
             // 
-            this.label1.ContextMenuStrip = this.contextMenuStrip1;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(375, 214);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "右键菜单控制";
+            label1.ContextMenuStrip = contextMenuStrip1;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(375, 214);
+            label1.TabIndex = 1;
+            label1.Text = "右键菜单控制";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 214);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.contextMenuStrip1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(375, 214);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "ClashTray";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
