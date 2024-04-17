6fb787b52f7fd667479461e3b64497afb5e098c6
JonasGao <gaopinsong@live.cn>
2024-04-17 14:32:10 +0800

    Single instance

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -2,16 +2,26 @@ namespace WinFormsApp1
 {
     internal static class Program
     {
+        const string AppId = "ClashTray/8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            using Mutex mutex = new(false, AppId);
+            if (mutex.WaitOne(0, false))
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.Run(new Form1());
+            }
+            else
+            {
+                MessageBox.Show("Application already running.");
+            }
         }
     }
 }
